0268d1bba0928d4ec0d07305cd0d53e2066e9bd0
hdela <hfdelacr@ucsd.edu>
2022-10-28 13:21:59 -0700

    ima dummy and had the images in my .gitignore. Changed it and updated webpack to use the available resources within the package.json to assign css and style loaders to not have a naked page.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,20 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT request to database being created, one moment please');
+  const jateDB = await openDB('jate', 1);
+  const text = jateDB.transaction('jate','readwrite');
+  const store = text.objectStore('jate');
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+  console.log('Saved to Databse ', result.value);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  
+}
+console.error('getDb not implemented');
 
 initdb();
