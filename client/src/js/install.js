3ed5762582b84a6dead1d578066c35461f591d5d
hdela <hfdelacr@ucsd.edu>
2022-10-30 15:52:10 -0700

    removing some comments

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,13 +1,11 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event; 
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt; 
     if(!promptEvent) {
@@ -19,7 +17,6 @@ butInstall.addEventListener('click', async () => {
     butInstall.classlist.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
 });
